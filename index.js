8a5a5799471a39234064047434153c7f243c78da
Nam Tran <namtran@Nams-MBP.hub>
2022-12-13 18:14:19 +1100

    some fixes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const PORT = 3003
+require('dotenv').config()
+const path = require('path')
+ 
+PORT = process.env.PORT || 3000
 const express = require('express')
 const {MongoClient} = require('mongodb')
 const cors = require('cors')
@@ -39,6 +42,12 @@ app.post('/location', async(req, res) => {
     }
 })
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 
 app.listen(PORT, () => console.log('Server running on PORT' + PORT))
